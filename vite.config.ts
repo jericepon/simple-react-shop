8b1e4c9aaa0e72a0cec20052d6da800fcfb9e034
Jeric Epon <jericepon@gmail.com>
2026-09-12 05:40:18 +0800

    Fix shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    tsconfigPaths: true,
+    alias: {
+      '@': '/src',
+    },
+  },
 })
